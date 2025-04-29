fd6ba1ce0f2008899c2c97cce361203d176cb9b6
Pranjal Raghuvanshi <118126928+Pranjal2506@users.noreply.github.com>
2025-04-29 17:54:15 +0530

    Update Audio.jsx

--- client/src/component/Audio.jsx
+++ client/src/component/Audio.jsx
@@ -53,7 +53,7 @@ const AudioRecorder = () => {
         formData.append("audio", audioBlob);
 
         try {
-            const response = await fetch("http://localhost:5000/transcribe", {
+            const response = await fetch("https://mock-interview-49z9.onrender.com/transcribe", {
                 method: "POST",
                 body: formData,
             });
